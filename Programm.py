214d97f1a3f06f5feb0a6a77f454295bb3179689
helendddd <mikheeva.el.24@gmail.com>
2023-09-14 14:43:05 +0300

    First Commit

---
+++ Programm.py
@@ -0,0 +1,14 @@
+t = int(input("Введите температуру: "))
+sms = input("Перевести в Цельсия/Фарингейта : (True/False) ")
+
+if sms == "True":
+    ans = (t - 32) / 1.8
+    ans = int(ans)
+    print (ans)
+elif sms == "False":
+    ans = t * 1.8 + 32
+    ans = int(ans)
+    print (ans)
+else:
+    print ("Неверные данные")
+
